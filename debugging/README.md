05ecd309fb604211641f7494b1f0b38460b30288
Akatsuki667 <qextzy@gmail.com>
2025-01-13 11:42:50 +0100

    Updtaing README.md, adding comments to factorial.py

--- debugging/README.md
+++ debugging/README.md
@@ -1,11 +1,13 @@
 # ChatGPT - Introduction
 
+## Description
+Use ChatGPT to identify and correct errors in code samples.
+
 ## TASKS
 
 ### 0. Debugging - Python Factorial
 
 #### Objectives
-- Use ChatGPT to identify and correct errors in code samples.
 - Fix the code, it should print the factorial of the argument.
 - Code to fix :
 
@@ -24,3 +26,19 @@ print(f)
 #### Expected result
 ➜  debugging git:(main) ✗ ./factorial.py 5
 Le factoriel de 5 est 120.
+
+
+### 1. Debugging - Python Arguments
+
+#### Objectives
+- Fix the code, it should print only the arguments without the python file name.
+- Code to fix :
+
+#!/usr/bin/python3
+import sys
+
+for i in range(len(sys.argv)):
+    print(sys.argv[i])
+
+#### Expected result
+
--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python3
-import sys
+#!/usr/bin/python3 # Shebang -> Utilisation interpréteur Python 3
+import sys # Accès arguments passés au script via sys.argv
 
-def factorial(n):
-    result = 1
+def factorial(n): # Déclaration fonction 
+    result = 1 # Initialisation variable
     while n > 1:
         result *= n
         n -= 1  # Réduction de n pour éviter une boucle infinie
     return result
 
 # Gestion des erreurs pour les entrées non valides
-try:
-    n = int(sys.argv[1])
-    if n < 0:
+try: # Débute bloc d'essai gestion exceptions
+    n = int(sys.argv[1]) # Lecture premier argument ligne et conversion de ce dernier
+    if n < 0: # Condition de vérification
         print("Erreur : le factoriel d'un nombre négatif n'est pas défini.")
     else:
-        f = factorial(n)
-        print(f"Le factoriel de {n} est {f}.")
-except (IndexError, ValueError):
-    print("Usage : ./script.py <entier>")
+        f = factorial(n) # Appel fonction pour calcul
+        print(f"Le factoriel de {n} est {f}.") # Affichage résultat
+except (IndexError, ValueError): # Gestion exceptions capture deux types d'erreurs
+    print("Usage : ./script.py <entier>") # Affichage message d'usage
